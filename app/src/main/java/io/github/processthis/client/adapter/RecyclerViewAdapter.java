06e5a80cc489e9028513fda0c7d27b9d24aaf5fa
nevdiaz <jenndiazz@icloud.com>
2019-08-05 17:56:34 -0600

    recycler view, featured layout, viewmodel

---
+++ app/src/main/java/io/github/processthis/client/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package io.github.processthis.client.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import io.github.processthis.client.R;
+import io.github.processthis.client.adapter.RecyclerViewAdapter.SketchHolder;
+import io.github.processthis.client.model.Sketch;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<SketchHolder> {
+  private Context context;
+  private List<Sketch> sketches;
+
+
+  public RecyclerViewAdapter(Context context,
+      List<Sketch> sketches) {
+    this.context = context;
+    this.sketches = sketches;
+  }
+
+  @NonNull
+  @Override
+  public SketchHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View view;
+    LayoutInflater Inflater = LayoutInflater.from(context);
+    view = Inflater.inflate(R.layout.cardview, parent, false);
+    return new SketchHolder(view);
+  }
+
+  // this tells the holder to display the sketch at position x
+  @Override
+  public void onBindViewHolder(@NonNull SketchHolder holder, int position) {
+    holder.bind(sketches.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return sketches.size();
+  }
+
+  class SketchHolder extends RecyclerView.ViewHolder{
+    TextView sketchName;
+    ImageView sketchThumbnail;
+
+   private SketchHolder(View itemView){
+     super(itemView);
+     sketchName = itemView.findViewById(R.id.sketch_title);
+     sketchThumbnail = itemView.findViewById(R.id.sketch_image);
+   }
+
+   private void bind (Sketch sketch){
+     sketchName.setText(sketch.getSketchTitle());
+//     sketchThumbnail.setImageDrawable(sketch.getThumbnail());
+     sketchThumbnail.setImageDrawable(context.getDrawable(R.drawable.sketch3));
+   }
+
+  }
+}
